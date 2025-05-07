96ead962efcbbd9c0a00ce65f367d33f96a60449
anamunizzz <2018analuizamunizsousa@gmail.com>
2025-05-06 22:37:06 -0300

    teste db.js

--- backend/db.js
+++ backend/db.js
@@ -1,1 +1,17 @@
 //<- Aqui ficará a conexão com o MySQL
+
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',         // ou seu usuário
+  password: '',         // sua senha
+  database: 'mei_help'  // nome do seu banco
+});
+
+connection.connect((err) => {
+  if (err) throw err;
+  console.log('Conectado ao MySQL!');
+});
+
+module.exports = connection;
